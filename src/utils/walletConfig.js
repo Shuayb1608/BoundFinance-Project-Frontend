d0d6fa2fb616ff4612877b3c37cbc9bf26925ad0
Shuayb1608 <124663422+Shuayb1608@users.noreply.github.com>
2023-08-12 23:05:18 +0100

    Update walletConfig.js

--- src/utils/walletConfig.js
+++ src/utils/walletConfig.js
@@ -5,7 +5,7 @@ import { goerli } from 'wagmi/chains'
 
 
 const chains = [ goerli ]
-export const projectId = process.env.PROJECT_WALLETCONNECT_ID;
+export const projectId = process.env.REACT_APP_PROJECT_ID;
 
 const { publicClient } = configureChains(chains, [w3mProvider({ projectId })])
 export const wagmiConfig = createConfig({
